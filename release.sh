d36e9cedb858b2c75fe25d12e0a0e0ca7965450f
phil294 <philip@waritschlager.de>
2023-05-18 12:29:54 +0200

    Revert "yarn upgrade"
    This reverts commit e1dc7ac590dc60321ccf6c78da74e6c7af86ef0f.

--- release.sh
+++ release.sh
@@ -12,18 +12,19 @@ if ! [ -z "$(git status --porcelain)" ]; then
     exit 1
 fi
 
-yarn upgrade
-pushd web
-yarn
-yarn upgrade
-popd
-git add yarn.lock
-git add package.json
-git add web/yarn.lock
-git add web/package.json
-git commit -m 'yarn upgrade' ||:
-echo yarn upgraded
-pause
+# # Cannot upgrade deps currently, TODO: https://github.com/vuejs/vue-loader/issues/2044
+# yarn upgrade
+# pushd web
+# yarn
+# yarn upgrade
+# popd
+# git add yarn.lock
+# git add package.json
+# git add web/yarn.lock
+# git add web/package.json
+# git commit -m 'yarn upgrade' ||:
+# echo yarn upgraded
+# pause
 
 yarn
 yarn coffee -c src/*.coffee
