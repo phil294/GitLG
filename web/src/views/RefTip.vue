2e6e5673cce4f0b7c1df5e8f274cc88c9f04f774
Philip Waritschlager <philip@waritschlager.de>
2024-11-19 01:58:52 +0100

    Various small fixes

--- web/src/views/RefTip.vue
+++ web/src/views/RefTip.vue
@@ -7,8 +7,8 @@
 	</div>
 </template>
 <script setup>
-import { computed } from 'vue';
-import { head_branch, combine_branches, branch_actions, stash_actions, tag_actions, selected_git_action, show_branch } from '../state/store.js';
+import { computed } from 'vue'
+import { head_branch, combine_branches, branch_actions, stash_actions, tag_actions, selected_git_action, show_branch } from '../state/store.js'
 
 let props = defineProps({
 	git_ref: {
