f122130764e5f08b6a98a32e880555e181be4feb
Philip Waritschlager <philip@waritschlager.de>
2024-11-29 21:27:43 +0100

    Upgrade vscode-elements

--- web/src/views/RepoSelection.vue
+++ web/src/views/RepoSelection.vue
@@ -4,21 +4,13 @@
 	</div>
 </template>
 <script setup>
-import { onMounted, useTemplateRef, watch } from 'vue'
+import { watch } from 'vue'
 import { stateful_computed, refresh_main_view } from '../state/store.js'
 
 let repo_names = stateful_computed('repo-names', ['Loading...'])
 /** @type {Vue.WritableComputedRef<number>} */
 let selection = stateful_computed('selected-repo-index', 0, () =>
 	watch(selection, () => refresh_main_view()))
-
-let select_ref = /** @type {Readonly<Vue.ShallowRef<HTMLElement|null>>} */ (useTemplateRef('select_ref')) // eslint-disable-line @stylistic/no-extra-parens
-onMounted(() => {
-	// https://github.com/vscode-elements/elements/issues/231
-	let extra_select_style = document.createElement('style')
-	extra_select_style.innerHTML = '.text { display: block; overflow: hidden; height: 18px; }'
-	select_ref.value?.shadowRoot?.appendChild(extra_select_style)
-})
 </script>
 <style>
 	vscode-single-select {
