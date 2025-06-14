ae3698b9891e8d2bb62c31bd0e6ca8b1da5fb683
Philip Waritschlager <philip@waritschlager.de>
2025-06-14 02:56:56 +0200

    fix new checkbox focus indicator

--- web/src/App.vue
+++ web/src/App.vue
@@ -240,6 +240,9 @@ input[type="checkbox"] {
 		background-color: var(--vscode-settings-checkboxBackground);
 		border: 1px solid var(--vscode-settings-checkboxBorder);
 	}
+	&:focus:after {
+		outline: 1px solid var(--vscode-focusBorder);
+	}
 	&:checked:after {
 		/* fill=currentColor doesn't work here... */
 		background-image: url('data:image/svg+xml;utf8,<svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="white"><path fill-rule="evenodd" clip-rule="evenodd" d="M14.431 3.323l-8.47 10-.79-.036-3.35-4.77.818-.574 2.978 4.24 8.051-9.506.764.646z"></path></svg>');
